651e32e87075cf8cc39d0aae320bbc08974dcd69
EugeneFaceControl <eugenefacecontrol@gmail.com>
2018-12-15 20:13:32 +0300

    App settings check

---
+++ Tests/SettigsChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFramework.Utils;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class SettigsChecker
+    {
+        [Test]
+        public void CheckSettings()
+        {
+            Console.WriteLine(TestSettings.ExplicitTimeout);
+            TestSettings.GetAllSettings();
+        }
+    }
+}
